4edf04358ae6a32341d36c26129cea3111df23a8
BlanahrAbarstyrn <ljspromo@protonmail.com>
2025-05-18 18:04:24 -0400

    more code added to textnode - CH2 / L1

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,24 @@
 from enum import Enum
 
-class TextType(enum):
+class TextType(Enum):
     pass
+
+class TextNode():
+    def __init__(text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        if url == "":
+            self.url = None
+        else:
+            self.url = url
+
+    def __eq__(self, other):
+        if isinstance(other, TextNode):
+            return (self.text == other.text and
+                    self.text_type == other.text_type and
+                    self.url == other.url)
+        return False
+    
+    def __repr__(self):
+        return f"{self.text}, {self.text_type}, {self.url}"
+    
